f6298dacf6c2c924e749ba69ff40a182267a1dbc
Jiaxin Wang <jiaxin@Jiaxins-MacBook-Pro.local>
2019-11-20 16:37:42 +0800

    update prior

--- imagine/priors/flat_prior.py
+++ imagine/priors/flat_prior.py
@@ -3,7 +3,6 @@ flat prior
 """
 
 import logging as log
-
 from imagine.priors.prior import Prior
 from imagine.tools.icy_decorator import icy
 
@@ -17,7 +16,16 @@ class FlatPrior(Prior):
     def __call__(self, cube):
         """
         return variable value as it is
-        :param cube: list of variable values in range [0,1]
-        :return: list of variable values in range [0,1]
+        
+        parameters
+        ----------
+        
+        cube
+            list of variable values in range [0,1]
+            
+        return
+        ------
+        list of variable values in range [0,1]
         """
+        log.debug('@ flat_prior::__call__')
         return cube
