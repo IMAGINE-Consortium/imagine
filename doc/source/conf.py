d9cb3bd1407b4498da14e2c3ba171d6a1eb11a5e
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2019-12-03 17:27:13 +0100

    RTD: more mock packages

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,7 +16,7 @@ import mock
 sys.path.insert(0, os.path.abspath('../../'))
 
 
-MOCK_MODULES = ['mpi4py']
+MOCK_MODULES = ['mpi4py', 'pymultinest','dynesty']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
