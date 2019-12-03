369e83ef0f2de073e24827298f44df9d541fbc03
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2019-12-03 17:20:05 +0100

    RTD: trying a mock to avoid problems installing mpi4py at RTD.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,7 +14,10 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-#import imagine
+
+MOCK_MODULES = ['mpi4py']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # -- Project information -----------------------------------------------------
 
