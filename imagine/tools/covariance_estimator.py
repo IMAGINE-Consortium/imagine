9f9e3523967e94a79b09f19e28c6669f886b0467
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2019-12-17 16:58:50 +0100

    Updates docstrings in covariance_estimator.py and removes repeated code.

--- imagine/tools/covariance_estimator.py
+++ imagine/tools/covariance_estimator.py
@@ -1,6 +1,7 @@
 """
-This module targets on estimation algorithms for covaraince matrix
-based on finite number of samples.
+This module contains estimation algorithms for the
+covariance matrix based on a finite number of samples.
+
 For the testing suits, please turn to "imagine/tests/tools_tests.py".
 """
 
@@ -9,16 +10,32 @@ from mpi4py import MPI
 import logging as log
 from imagine.tools.mpi_helper import mpi_mean, mpi_trans, mpi_mult, mpi_eye, mpi_trace
 
-
 comm = MPI.COMM_WORLD
 mpisize = comm.Get_size()
 mpirank = comm.Get_rank()
 
 def empirical_cov(data):
-    """
-    empirical covariance estimator
-    for distributed data with multiple global rows
-    (probably) the worst option
+    r"""
+    Empirical covariance estimator
+
+
+    Given some data matrix, :math:`D`, where rows are different samples
+    and columns different properties, the covariance can be
+    estimated from
+
+    .. math::
+           U_{ij} = D_{ij} -  \overline{D}_j\,,\;
+           \text{with}\; \overline{D}_j=\tfrac{1}{N} \sum_{i=1}^N D_{ij}
+
+    .. math::
+          \text{cov} = \tfrac{1}{N} U^T U
+
+
+
+    Notes
+    -----
+        While conceptually simple, this is usually not the
+        best option.
 
     Parameters
     ----------
@@ -27,22 +44,49 @@ def empirical_cov(data):
 
     Returns
     -------
-    numpy.ndarray
+    cov : numpy.ndarray
         distributed (not copied) covariance matrix in global shape
         (data size, data size) each node takes part of the rows
     """
     log.debug('@ covariance_estimator::empirical_cov')
     assert isinstance(data, np.ndarray)
     assert (len(data.shape) == 2)
-    # get ensemble size
+    # Get ensemble size (i.e. the number of rows)
     ensemble_size = np.array(0, dtype=np.uint)
-    comm.Allreduce([np.array(data.shape[0], dtype=np.uint), MPI.LONG], [ensemble_size, MPI.LONG], op=MPI.SUM)
-    u = data - mpi_mean(data)  # copied mean
-    return mpi_mult(mpi_trans(u), u) / ensemble_size
+    comm.Allreduce([np.array(data.shape[0], dtype=np.uint),
+                    MPI.LONG], [ensemble_size, MPI.LONG],
+                   op=MPI.SUM)
+    # Calculates covariance
+    u = data - mpi_mean(data)
+    cov = mpi_mult(mpi_trans(u), u) / ensemble_size
+    return cov
 
 def oas_cov(data):
-    """
-    OAS covariance estimator, prepared for examples
+    r"""
+    Estimate covariance with the Oracle Approximating Shrinkage algorithm.
+
+    Given some :math:`n\times m` data matrix, :math:`D`,
+    where rows are different samples and columns different properties,
+    the covariance can be estimated in the following way.
+
+    .. math::
+           U_{ij} = D_{ij} -  \overline{D}_j\,,\;
+           \text{with}\; \overline{D}_j=\tfrac{1}{N} \sum_{i=1}^N D_{ij}
+
+    Let
+
+    .. math::
+          S = \tfrac{1}{N} U^T U\,,\;
+          t = \text{tr}(S)\quad\text{and}\quad v = \text{tr}(S)
+
+    .. math::
+          \tilde\rho = \min\left[1,\frac{(1-2/m)v + t^2}{ (m+1-2/m)(v-t^2/m)}\right]
+
+    The covariance is given by
+
+    .. math::
+          \text{cov}_\text{OAS} = (1-\rho)S + \tfrac{1}{N} t \rho I_m
+
 
     Parameters
     ----------
@@ -51,33 +95,20 @@ def oas_cov(data):
 
     Returns
     -------
-    numpy.ndarray
+    cov : numpy.ndarray
         covariance matrix in global shape (data_size, data_size)
     """
     log.debug('@ covariance_estimator::oas_cov')
-    assert isinstance(data, np.ndarray)
-    assert (len(data.shape) == 2)
-    # data size
-    data_size = data.shape[1]
-    # ensemble size
-    ensemble_size = np.array(0, dtype=np.uint)
-    comm.Allreduce([np.array(data.shape[0], dtype=np.uint), MPI.LONG], [ensemble_size, MPI.LONG], op=MPI.SUM)
-    # calculate OAS covariance extimator from empirical covariance estimator
-    u = data - mpi_mean(data)
-    s = mpi_mult(mpi_trans(u), u) / ensemble_size
-    trs = mpi_trace(s)
-    trs2 = mpi_trace(mpi_mult(s, s))
-    numerator = (1.0-2.0/data_size)*trs2+trs*trs
-    denominator = (ensemble_size+1.0-2.0/data_size)*(trs2-(trs*trs)/data_size)
-    if denominator == 0:
-        rho = 1
-    else:
-        rho = np.min([1, numerator/denominator])
-    return (1.-rho)*s+mpi_eye(data_size)*rho*trs/data_size
+    _, cov = oas_mcov(data)
+
+    return cov
 
 def oas_mcov(data):
     """
-    OAS covariance estimator, prepared for Likelihood
+    Estimate covariance with the Oracle Approximating Shrinkage algorithm.
+
+    See `imagine.tools.covariance_estimator.oas_cov` for details. This
+    function aditionally returns the computed ensemble mean.
 
     Parameters
     ----------
@@ -86,29 +117,35 @@ def oas_mcov(data):
 
     Returns
     -------
-    mean : float
+    mean : numpy.ndarray
         copied ensemble mean (on all nodes)
-    numpy.ndarray
+    cov : numpy.ndarray
         distributed covariance matrix in shape (data_size, data_size)
     """
     log.debug('@ covariance_estimator::oas_mcov')
     assert isinstance(data, np.ndarray)
     assert (len(data.shape) == 2)
-    # data size
+
+    # Finds ensemble size and data size
     data_size = data.shape[1]
-    # ensemble size
     ensemble_size = np.array(0, dtype=np.uint)
-    comm.Allreduce([np.array(data.shape[0], dtype=np.uint), MPI.LONG], [ensemble_size, MPI.LONG], op=MPI.SUM)
-    # calculate OAS covariance extimator from empirical covariance estimator
+    comm.Allreduce([np.array(data.shape[0], dtype=np.uint), MPI.LONG],
+                   [ensemble_size, MPI.LONG], op=MPI.SUM)
+
+    # Calculates OAS covariance extimator from empirical covariance estimator
     mean = mpi_mean(data)
     u = data - mean
     s = mpi_mult(mpi_trans(u), u) / ensemble_size
     trs = mpi_trace(s)
     trs2 = mpi_trace(mpi_mult(s, s))
-    numerator = (1.0-2.0/data_size)*trs2+trs*trs
-    denominator = (ensemble_size+1.0-2.0/data_size)*(trs2-(trs*trs)/data_size)
+
+    numerator = (1.0 - 2.0/data_size)*trs2 + trs*trs
+    denominator = (ensemble_size +1.0-2.0/data_size)*(trs2 - (trs*trs)/data_size)
+
     if denominator == 0:
         rho = 1
     else:
         rho = np.min([1, numerator/denominator])
-    return mean, (1.-rho)*s+mpi_eye(data_size)*rho*trs/data_size
+    cov = (1.-rho)*s+mpi_eye(data_size)*rho*trs/data_size
+
+    return mean, cov
