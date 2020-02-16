7188fdd7973526bafc7150c6e36a2c50de18fc41
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2020-02-16 10:30:13 +0100

    Minor: fixes spacing.

--- imagine/observables/dataset.py
+++ imagine/observables/dataset.py
@@ -53,100 +53,98 @@ class HEALPixDataset(Dataset):
 
 @icy
 class FaradayDepthHEALPixDataset(HEALPixDataset):
-      r"""
-      Stores a Faraday depth map into IMAGINE-compatible
-      dataset
+    r"""
+    Stores a Faraday depth map into IMAGINE-compatible
+    dataset
 
-      Parameters
-      ----------
-      data : numpy.ndarray
-          1D-array containing the HEALPix map
-      Nside : int, optional
-          For extra internal consistency checking. If `Nside` is present,
-          it will be checked whether :math:`12\times N_{side}^2` matches
+    Parameters
+    ----------
+    data : numpy.ndarray
+        1D-array containing the HEALPix map
+    Nside : int, optional
+        For extra internal consistency checking. If `Nside` is present,
+        it will be checked whether :math:`12\times N_{side}^2` matches
 
-      Attributes
-      -------
-      data
-          Data in ObservablesDict-compatible shape
-      key
-          Standard key associated with this observable
-      """
-      @property
-      def name(self):
-          return 'fd'
+    Attributes
+    -------
+    data
+        Data in ObservablesDict-compatible shape
+    key
+        Standard key associated with this observable
+    """
+    @property
+    def name(self):
+        return 'fd'
 
 @icy
 class DispersionMeasureHEALPixDataset(HEALPixDataset):
-      r"""
-      Stores a Dispersion measure map into IMAGINE-compatible
-      dataset
+    r"""
+    Stores a Dispersion measure map into IMAGINE-compatible
+    dataset
 
-      Parameters
-      ----------
-      data : numpy.ndarray
-          1D-array containing the HEALPix map
-      Nside : int, optional
-          For extra internal consistency checking. If `Nside` is present,
-          it will be checked whether :math:`12\times N_{side}^2` matches
+    Parameters
+    ----------
+    data : numpy.ndarray
+        1D-array containing the HEALPix map
+    Nside : int, optional
+        For extra internal consistency checking. If `Nside` is present,
+        it will be checked whether :math:`12\times N_{side}^2` matches
 
-      Attributes
-      -------
-      data
-          Data in ObservablesDict-compatible shape
-      key
-          Standard key associated with this observable
-      """
-      @property
-      def name(self):
-          return 'dm'
+    Attributes
+    -------
+    data
+        Data in ObservablesDict-compatible shape
+    key
+        Standard key associated with this observable
+    """
+    @property
+    def name(self):
+        return 'dm'
 
 @icy
 class SynchrotronHEALPixDataset(HEALPixDataset):
-      r"""
-      Stores a synchrotron emission maps into IMAGINE-compatible
-      dataset. These include Stokes parameters, total and polarised
-      intensity, and polarisation angle.
+    r"""
+    Stores a synchrotron emission maps into IMAGINE-compatible
+    dataset. These include Stokes parameters, total and polarised
+    intensity, and polarisation angle.
 
-      The parameter `type` and the units of the map in `data` must follow:
+    The parameter `type` and the units of the map in `data` must follow:
 
-      * 'I' - total intensity (in unit K-cmb)
-      * 'Q' - Stokes Q (in unit K-cmb, IAU convention)
-      * 'U' - Stokes U (in unit K-cmb, IAU convention)
-      * 'PI' - polarisation intensity (in unit K-cmb)
-      * 'PA' - polarisation angle (in unit rad, IAU convention)
-
-
-      Parameters
-      ----------
-      data : numpy.ndarray
-          1D-array containing the HEALPix map
-      frequency : float
-          Frequency of the radio observation in $\rm cm$
-
-      Nside : int, optional
-          For extra internal consistency checking. If `Nside` is present,
-          it will be checked whether :math:`12\times N_{side}^2` matches data.size
-      type : str
-          The type of map being supplied in `data`.
+    * 'I' - total intensity (in unit K-cmb)
+    * 'Q' - Stokes Q (in unit K-cmb, IAU convention)
+    * 'U' - Stokes U (in unit K-cmb, IAU convention)
+    * 'PI' - polarisation intensity (in unit K-cmb)
+    * 'PA' - polarisation angle (in unit rad, IAU convention)
 
 
-      Attributes
-      -------
-      data
-          Data in ObservablesDict-compatible shape
-      key
-          Standard key associated with this observable
-      """
-      def __init__(self, data, frequency, type, Nside=None):
-          super().__init__(data, Nside)
+    Parameters
+    ----------
+    data : numpy.ndarray
+      1D-array containing the HEALPix map
+    frequency : float
+      Frequency of the radio observation in $\rm cm$
 
-          self.frequency = str(frequency)
-          # Checks whether the type is valid
-          assert type in ['I','Q','U','PI','PA']
-          self.tag = type
-      @property
-      def name(self):
-          return 'sync'
+    Nside : int, optional
+      For extra internal consistency checking. If `Nside` is present,
+      it will be checked whether :math:`12\times N_{side}^2` matches data.size
+    type : str
+      The type of map being supplied in `data`.
 
 
+    Attributes
+    -------
+    data
+      Data in ObservablesDict-compatible shape
+    key
+      Standard key associated with this observable
+    """
+    def __init__(self, data, frequency, type, Nside=None):
+        super().__init__(data, Nside)
+
+        self.frequency = str(frequency)
+        # Checks whether the type is valid
+        assert type in ['I','Q','U','PI','PA']
+        self.tag = type
+    @property
+    def name(self):
+        return 'sync'
