47232f5c019db49f0f5c00a463bec2bb23fee572
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2020-06-15 11:24:17 +0200

    Minor adjustments to config.py.

--- imagine/tools/config.py
+++ imagine/tools/config.py
@@ -1,29 +1,35 @@
-""" 
+"""
 This module is responsible for setting the `rc` configuration variables.
 
-These can be accessed and modified using the 
-:py:data:`imagine.rc <imagine.config.rc>` dictionary or setting the 
-corresponding environment variables (named 'IMAGINE_'+RC_VAR_NAME). 
+These can be accessed and modified using the
+:py:data:`imagine.rc <imagine.config.rc>` dictionary or setting the
+corresponding environment variables (named 'IMAGINE_'+RC_VAR_NAME).
 """
 import tempfile
 import os
 
 # Sets default values of configuration parameters
-rc = {'temp_dir': None, 'test':0}
+rc = {'temp_dir': None}
 
 
+def read_rc_from_env():
+    """
+    Updates the rc configuration dictionary using current
+    environment variables
+    """
+    global rc
+    for var in rc:
+        env_var = 'IMAGINE_'+var.upper()
 
-for var in rc:
-    env_var = 'IMAGINE_'+var.upper()
-
-    try:
-        rc[var] = os.environ[env_var]
-    except KeyError:
-        pass
+        try:
+            rc[var] = os.environ[env_var]
+        except KeyError:
+            pass
 
 
-# If a temp directory was not set using envirnment variables, create one
+read_rc_from_env()
+
+# If a temp directory was not set using environment variables, create one
 if rc['temp_dir'] is None:
     rc['temp_dir_obj'] = tempfile.TemporaryDirectory(prefix='imagine_')
-    # Default values that require previous values
     rc['temp_dir'] = rc['temp_dir_obj'].name
