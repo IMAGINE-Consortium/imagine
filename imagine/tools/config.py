d13ccb82249c54a174bea018d414d3e476111cd3
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2020-06-15 16:57:44 +0200

    Converts environment variable to python type automatically.

--- imagine/tools/config.py
+++ imagine/tools/config.py
@@ -9,7 +9,30 @@ import tempfile
 import os
 
 # Sets default values of configuration parameters
-rc = {'temp_dir': None}
+rc = {'temp_dir': None,
+      'distributed_arrays': False}
+
+
+def _str_to_python(v):
+    """
+    Attempts to convert a string to a python basic type
+    """
+    # Tries to convert to a number
+    try:
+        v = float(v)
+        # Converts to integer if needed
+        if v.is_integer():
+            v = int(v)
+    except ValueError:
+        pass
+
+    # Converts to boolean if needed
+    if v in ('True','T','TRUE'):
+        v = True
+    elif v in ('False','F','FALSE'):
+        v = False
+
+    return v
 
 
 def read_rc_from_env():
@@ -22,7 +45,7 @@ def read_rc_from_env():
         env_var = 'IMAGINE_'+var.upper()
 
         try:
-            rc[var] = os.environ[env_var]
+            rc[var] = _str_to_python(os.environ[env_var])
         except KeyError:
             pass
 
