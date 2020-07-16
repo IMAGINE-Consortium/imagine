efd42d50beb77489e9eaa737bedc9e3a7b052698
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2020-07-16 21:33:11 +0200

    Removes ellipsis from templates.

--- imagine/templates/field_factory_template.py
+++ imagine/templates/field_factory_template.py
@@ -9,10 +9,8 @@ class YourFieldFactory(FieldFactory):
 
     # Default values are used for inactive parameters
     DEFAULT_PARAMETERS = {'param_A': param_A_value,
-                          'param_B': param_B_value,
-                          ... }
+                          'param_B': param_B_value}
 
     # All parameters need a range and a prior
     PRIORS = {'param_A': PriorA(prior_args, interval=[A_min, A_max])
-              'param_B': PriorB(prior_args, interval=[B_min, B_max])
-              ... }
+              'param_B': PriorB(prior_args, interval=[B_min, B_max])}
