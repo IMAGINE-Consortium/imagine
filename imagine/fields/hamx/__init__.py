2eb628d130d2740fc252e3da6f1243ffae54a442
ashley-stock <stock@astro.utoronto.ca>
2020-10-21 17:22:00 -0400

    Added Jansson & Farrar Hammurabi model

--- imagine/fields/hamx/__init__.py
+++ imagine/fields/hamx/__init__.py
@@ -3,15 +3,19 @@
 # %% IMPORTS
 # Import base modules
 from . import (
-    breg_lsa, brnd_es, cre_analytic, tereg_ymw16)
+    breg_lsa, breg_jf12, brnd_es, brnd_jf12, cre_analytic, tereg_ymw16)
 from .breg_lsa import *
+from .breg_jf12 import *
 from .brnd_es import *
+from .brnd_jf12 import *
 from .cre_analytic import *
 from .tereg_ymw16 import *
 
 # All declaration
-__all__ = ['breg_lsa', 'brnd_es', 'cre_analytic', 'tereg_ymw16']
+__all__ = ['breg_lsa', 'breg_jf12', 'brnd_es', 'brnd_jf12','cre_analytic', 'tereg_ymw16']
 __all__.extend(breg_lsa.__all__)
+__all__.extend(breg_jf12.__all__)
 __all__.extend(brnd_es.__all__)
+__all__.extend(brnd_jf12.__all__)
 __all__.extend(cre_analytic.__all__)
 __all__.extend(tereg_ymw16.__all__)
