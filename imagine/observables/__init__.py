1557777e6983fb16b98b82a085c8831ce3e7f06c
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2020-11-24 08:40:21 +0100

    Includes tools for checking likelihood convergence First step to finally address issue #21

--- imagine/observables/__init__.py
+++ imagine/observables/__init__.py
@@ -3,13 +3,15 @@
 # %% IMPORTS
 # Import base modules
 from . import (
-    dataset, observable, observable_dict)
+    dataset, observable, observable_dict, toolkit)
 from .dataset import *
 from .observable import *
 from .observable_dict import *
+from .toolkit import *
 
 # All declaration
-__all__ = ['dataset', 'observable', 'observable_dict']
+__all__ = ['dataset', 'observable', 'observable_dict', 'toolkit']
 __all__.extend(dataset.__all__)
 __all__.extend(observable.__all__)
 __all__.extend(observable_dict.__all__)
+__all__.extend(toolkit.__all__)
