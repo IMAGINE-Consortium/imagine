9b50897c4e0d4c790a70aa42600e433ac31ab253
YSloots <yourisloots@gmail.com>
2022-10-24 10:14:04 +0200

    Made AddField() magnetfield type specific

--- imagine/fields/field_utility.py
+++ imagine/fields/field_utility.py
@@ -14,17 +14,18 @@ import astropy.units as u
 
 # IMAGINE imports
 from imagine.fields import Field
+from imagine.fields import MagneticField
 from imagine.tools import req_attr
 
 # All declaration
-__all__ = ['FieldAdder']
+__all__ = ['FieldAdder', 'ArrayMagneticField']
 
 
 class FieldAdder(Field):
 
     def __init__(self, grid, summand_1, summand_2, parameters={}, ensemble_size=None,
                  ensemble_seeds=None, dependencies={}):
-
+        
         if summand_1.grid != summand_2.grid:
             raise ValueError('Fields can only be added if defined on the same grid')
         if summand_1.TYPE != summand_2.TYPE:
@@ -52,6 +53,63 @@ class FieldAdder(Field):
 
     def compute_field(self, seed):
         return self.summand_1.compute_field(seed) + self.summand_2.compute_field(seed)
+
+
+class ArrayMagneticField(MagneticField):
+
+    def __init__(self, grid, array_field, scale, name, parameters={}, ensemble_size=None,
+                 ensemble_seeds=None, dependencies={}):
+
+	# Possible checks:
+		# grid must be used the generate the array
+        self.array_field = array_field
+        self.NAME        = name
+        self.PARAMETER_NAMES = [name+'_scale']
+
+        super().__init__(grid, parameters={name+'_scale':scale}, ensemble_size=None,
+                         ensemble_seeds=None, dependencies={})
+
+    @property
+    def data_shape(self):
+        return(self.array_field.shape)
+
+    def compute_field(self, seed):
+        return self.parameters[self.NAME+'_scale']*self.array_field
+
+"""
+
+class ArrayField(Field):
+
+    def __init__(self, array_field, config={}, parameters={}, ensemble_size=None,
+                 ensemble_seeds=None, dependencies={}):
+
+	# Possible checks:
+		# grid must be used the generate the array
+		# config must contain all field attributes
+		# type must be a valid imagine base_field type
+        self.array_field = array_field
+        self.UNITS = config['units']
+        self.TYPE  = config['type']
+        self.NAME  = config['field_name']
+        self.PARAMETER_NAMES = [config['field_name']+'_scale']
+        
+        # problem here is that we need the method data_discription from base_fields
+        # and we only know what type the field is upon init so need ugly tests
+        self._data_description = field.data_description
+
+        super().__init__(config['grid'], parameters={}, ensemble_size=None,
+                         ensemble_seeds=None, dependencies={})
+
+    @property
+    def data_description(self):
+        return(self._data_description())
+
+    @property
+    def data_shape(self):
+        return(self.array.shape())
+
+    def compute_field(self, seed):
+        return self.parameters[self.PARAMETER_NAMES[0]]*self.array_field
 
 
 class ArrayField(Field):
@@ -83,4 +141,6 @@ class ArrayField(Field):
 
     def compute_field(self, seed):
         return self.parameters[self.PARAMETER_NAMES[0]]*self.array
+        
+"""
 
