675a88f736923b9aabc2d15c98e3beda9cc833e9
shutsch <hutsch@astro.ru.nl>
2023-01-10 17:04:22 +0100

    Adapted jf12 to accept astropy units

--- imagine/fields/cwrapped/wrappedjf12.py
+++ imagine/fields/cwrapped/wrappedjf12.py
@@ -1,5 +1,7 @@
 import ImagineModels as im
 import numpy as np
+import astropy.units as u
+import astropy as ap
 
 from imagine.fields.base_fields import MagneticField
 from imagine.fields.field_factory import FieldFactory
@@ -13,11 +15,16 @@ class WrappedJF12(MagneticField):
     PARAMETER_NAMES = ['b_arm_1', 'b_arm_2', 'b_arm_3', 'b_arm_4', 'b_arm_5', 'b_arm_6',  'b_arm_7', 'b_ring', 'h_disk',
                        'w_disk', 'Bn', 'Bs', 'rn', 'rs', 'wh', 'z0', 'B0_X', 'Xtheta_const', 'rpc_X', 'r0_X', ]
 
+    DEFAULT_UNITS =  {'b_arm_1': u.microgauss, 'b_arm_2': u.microgauss, 'b_arm_3': u.microgauss, 'b_arm_4': u.microgauss, 'b_arm_5': u.microgauss,
+                      'b_arm_6': u.microgauss, 'b_arm_7': u.microgauss, 'b_ring': u.microgauss, 'h_disk': u.kpc, 'w_disk': u.kpc,
+                      'Bn': u.microgauss, 'Bs': u.microgauss, 'rn':  u.kpc, 'rs': u.kpc, 'wh':  u.kpc, 'z0':  u.kpc, 'B0_X': u.microgauss,
+                      'Xtheta_const': u.rad, 'rpc_X': u.kpc, 'r0_X': u.kpc, }
+
     def compute_field(self, seed):
         # TODO: tolist (and hence unnecessary copy) is needed due to wrapper implementation, should change in the future
 
         res = self.grid.resolution
-        
+
         x = self.grid.box[0]#.to_value()
         x = np.linspace(x[0].value, x[1].value, res[0]).tolist()
         y = self.grid.box[1]#.to_value()
@@ -30,17 +37,19 @@ class WrappedJF12(MagneticField):
 
         for key, val in self.parameters.items():
             if hasattr(wrapped_jf12, key):
+                if isinstance(val, ap.Quantity):
+                    val = val.to(self.DEFAULT_UNITS[key]).value
                 setattr(wrapped_jf12, key, val)
 
         # TODO: npasarray is needed due to wrapper implementation, should change in the future
         B = np.asarray(wrapped_jf12.evaluate_grid(grid_x=x, grid_y=y, grid_z=z))*self.UNITS
         #print("Found {} nans inside JF12 field".format(np.sum(np.isnan(B))))
-        
+
         # Test B for nans
         #if np.sum(np.isnan(B)) != 0:
             #print("Setting {} nans to 0".format(np.sum(np.isnan(B))))
             #B[np.isnan(B)] = 0*self.UNITS
-        
+
         return B
 
 
